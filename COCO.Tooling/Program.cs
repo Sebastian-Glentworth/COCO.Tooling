37c7a0868b75b6d05f3c8105e761a9f7632018e1
Henry Senior <hps1945@live.co.uk>
2020-04-09 12:24:37 +0100

    Added val2014 model and helper functions

--- COCO.Tooling/Program.cs
+++ COCO.Tooling/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace COCO.Tooling
 {
@@ -6,7 +8,42 @@ namespace COCO.Tooling
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            new Program();
+        }
+
+        private Program()
+        {
+
+        }
+
+        private List<FileInfo> LoadImagesIntoList(string location)
+        {
+            var directoryInfo = new DirectoryInfo(location);
+            var files = directoryInfo.GetFiles("*.jpg");
+
+            return new List<FileInfo>(files);
+        }
+
+        private List<FileInfo> SelectRandomImages(List<FileInfo> dataset, int count)
+        {
+            var randomImages = new List<FileInfo>();
+
+            for(int i = 0; i < count; i++)
+            {
+                Random rnd = new Random();
+                int index = rnd.Next(0, dataset.Count);
+
+                randomImages.Add(dataset[index]);
+            }
+
+            return randomImages;
+        }
+
+        private string GetCaptionFromImage(FileInfo image, string JsonFileLocation)
+        {
+
+
+            return string.Empty;
         }
     }
 }

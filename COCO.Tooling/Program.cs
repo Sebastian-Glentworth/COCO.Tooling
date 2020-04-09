0d9791f14cfde57fa060e94c06bcc7530e3dc810
Henry Senior <hps1945@live.co.uk>
2020-04-09 12:03:22 +0100

    Added empty .netcore console project

---
+++ COCO.Tooling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COCO.Tooling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
